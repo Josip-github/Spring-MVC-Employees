dbe94b9a0136bbdff4e3d99b745903e5fb8881fd
Josip-github <josipcota1@gmail.com>
2021-10-23 17:56:27 +0200

    This commit creates class EmployeeController The purpose of the method showEmployees() is to display a list of all employees.

---
+++ src/main/java/com/example/springbootwebapp/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.springbootwebapp.controller;
+
+import com.example.springbootwebapp.model.Employee;
+import com.example.springbootwebapp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/showEmployees")
+    public ModelAndView showEmployees(){
+        ModelAndView mav = new ModelAndView("list-employees");
+        List<Employee> list = employeeRepository.findAll();
+        mav.addObject("employees", list);
+        return mav;
+    }
+}
